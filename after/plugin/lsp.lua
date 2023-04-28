b2fcbdeb4d6ee9f6a69691ab59fc1f240bf3206c
chayandatta <chayan.dutta1996@gmail.com>
2023-04-29 04:19:43 +0530

    more lsp configs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,12 @@ local lsp = require('lsp-zero')
 lsp.preset("recommended")
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+	local opts = {buffer = bufnr, remap = false}
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_reference, {})
+
 end)
 
 lsp.ensure_installed({
@@ -16,3 +21,7 @@ lsp.ensure_installed({
 })
 
 lsp.setup()
+
+vim.diagnostic.config({
+	virtual_text = true
+})
