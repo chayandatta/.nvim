aea28eeff309f4e30377d1f9ef99a02206873156
chayandatta <chayan.dutta1996@gmail.com>
2023-05-01 00:42:31 +0530

    bug fix

--- lua/chayan/remap.lua
+++ lua/chayan/remap.lua
@@ -36,7 +36,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
-
